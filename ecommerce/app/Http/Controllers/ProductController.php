2ed481cdf313f416d0ddd5549dc6116b8402f3c4
boukhezzarfahim <boukhezzarfahim3@gmail.com>
2024-06-02 21:18:42 +0100

    route

--- ecommerce/app/Http/Controllers/ProductController.php
+++ ecommerce/app/Http/Controllers/ProductController.php
@@ -10,18 +10,13 @@ use App\Http\Requests\ProductRequest;
 
 class ProductController extends Controller
 {
-
-/**
+ /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $products = Product::query()->paginate(10);
-<<<<<<< HEAD
+        $products = Product::query()->with('category')->paginate(10);
         return view('users.admin.product.index', compact(
-=======
-        return view('users.admin.product.index', compact(
->>>>>>> 8c09309d71aabf58d9177d22315ee98ca7fa06ee
             'products'
         ));
     }
@@ -38,11 +33,7 @@ class ProductController extends Controller
             'price' => 0,
         ]);
         $isUpdate = false;
-<<<<<<< HEAD
         return view('users.admin.product.form', compact(
-=======
-        return view('product.form', compact(
->>>>>>> 8c09309d71aabf58d9177d22315ee98ca7fa06ee
             'product', 'isUpdate', 'categories'
         ));
     }
@@ -50,19 +41,17 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-
-     public function store(ProductRequest $request)
-     {
-         $formFields = $request->validated();
-         
-         if ($request->hasFile('image')) {
+    public function store(ProductRequest $request)
+    {
+        $formFields = $request->validated();
+        if ($request->hasFile('image')) {
             $formFields['image'] = $request->file('image')->store('product', 'public');
         }
 
-         Product::create($formFields);
+        Product::create($formFields);
 
-         return to_route('products.index')->with('success', 'Product created successfully');
-     }
+        return to_route('products.index')->with('success', 'Product created successfully');
+    }
 
     /**
      * Display the specified resource.
@@ -79,11 +68,7 @@ class ProductController extends Controller
     {
         $isUpdate = true;
         $categories = Category::all();
-<<<<<<< HEAD
         return view('users.admin.product.form', compact(
-=======
-        return view('product.form', compact(
->>>>>>> 8c09309d71aabf58d9177d22315ee98ca7fa06ee
             'product', 'isUpdate', 'categories'
         ));
     }
@@ -93,14 +78,8 @@ class ProductController extends Controller
      */
     public function update(ProductRequest $request, Product $product)
     {
-
-        $formFields = $request->validated();
-
-
-        $product->fill($formFields)->save();
-
-
-        return redirect()->route('products.index')->with('success', 'Product updated successfully');
+        $product->fill($request->validated())->save();
+        return to_route('products.index')->with('success', 'Product updated successfully');
     }
 
     /**
--- ecommerce/resources/views/users/admin/product/index.blade.php
+++ ecommerce/resources/views/users/admin/product/index.blade.php
@@ -4,64 +4,58 @@
 @section('content')
     <div class="d-flex justify-content-between align-items-center">
         <h1>Product list </h1>
-        <a href="{{ route('products.create') }}" class="btn btn-primary">Create</a>
+        <a href="{{route('products.create')}}" class="btn btn-primary">Create</a>
     </div>
     <table class="table">
         <thead>
-            <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Descriptions</th>
-                <th>Quantity</th>
-                <th>Image</th>
-                <th>Category</th>
-                <th>Price</th>
-                <th>Operations</th>
-            </tr>
+        <tr>
+            <th>#ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>Quantity</th>
+            <th>Image</th>
+            <th>Price</th>
+            <th>Actions</th>
+        </tr>
         </thead>
         <tbody>
-            @forelse($products as $product)
-                <tr>
-                    <td>{{ $product->id }}</td>
-                    <td>{{ $product->name }}</td>
-                    <td>{{ $product->description }}</td>
-                    <td>{{ $product->quantity }}</td>
-                    <td><img width="100px" src="storage/{{ $product->image }}" alt=""></td>
+        @forelse($products as $product)
+            <tr>
+                <td>{{$product->id}}</td>
+                <td>{{$product->name}}</td>
+                <td>{!!$product->description  !!}</td>
+                <td align="center">
 
-<<<<<<< HEAD:ecommerce/resources/views/users/admin/product/index.blade.php
-                    <td> 
-                        <a class="" href="{{route('users.admin.categories.show' , $product->category_id)}}"> 
-                        <span class="badge bg-success">
-                            {{ $product->category->name }}
-                       </span>
-                    </a>
+                    @if($product->category)
+                        <a href="{{route('categories.show', $product->category_id)}}" class="btn btn-link">
+                        <span class="badge bg-primary">
+                          {{$product->category->name}}
+                        </span>
+                        </a>
+                    @endif
                 </td>
-=======
-                    <td> <span class="badge bg-success">{{ $product->category->name }}</span></td>
->>>>>>> 8c09309d71aabf58d9177d22315ee98ca7fa06ee:ecommerce/resources/views/product/index.blade.php
+                <td>{{$product->quantity}}</td>
+                <td><img width="100px" src="storage/{{$product->image}}" alt=""></td>
+                <td>{{$product->price}} MAD</td>
+                <td>
+                    <div class="btn-group gap-2">
+                        <a href="{{route('products.edit', $product)}}" class="btn btn-primary">Update</a>
+                        <form method="post" action="{{route('products.destroy', $product)}}">
+                            @csrf
+                            @method('DELETE')
+                            <input type="submit" class="btn btn-danger" value="Delete"/>
+                        </form>
+                    </div>
+                </td>
+            </tr>
+        @empty
+            <tr>
+                <td colspan="6" align="center"><h6>No products.</h6></td>
+            </tr>
 
-                    <td>{{ $product->price }} DZA</td>
-
-                        <th>
-                            <div class="btn-group gap-2">
-                                <a href="{{route('products.edit', $product)}}" class="btn btn-primary">Update</a>
-                                <form method="post" action="{{route('products.destroy' , $product)}}">
-                                    @csrf
-                                    @method('DELETE')
-                        <button wire:click="deleteProduct({{ $product->id }})" class="btn btn-danger">Delete</button>
-                                </form>
-                            </div>
-                        </th>
-
-                </tr>
-            @empty
-                <tr>
-                    <td colspan="6" align="center">
-                        <h6>No products</h6>
-                    </td>
-                </tr>
-            @endforelse
+        @endforelse
         </tbody>
     </table>
-    {{ $products->links() }}
+    {{$products->links()}}
 @endsection
